001cb6eed4b7b08afd6c99dc6b9931ee64084242
svost <ya.nowa@yandex.ru>
2025-01-12 22:49:42 +0300

    Set _WIN32_WINNT to windows 7

--- src/compat.h
+++ src/compat.h
@@ -6,7 +6,9 @@
 #define _BITCOIN_COMPAT_H 1
 
 #ifdef WIN32
-#define _WIN32_WINNT 0x0501
+#if (_WIN32_WINNT != _WIN_WINNT_WIN7)
+#define _WIN32_WINNT 0x601
+#endif
 #define WIN32_LEAN_AND_MEAN 1
 #ifndef NOMINMAX
 #define NOMINMAX
@@ -17,7 +19,6 @@
 #endif
 #define FD_SETSIZE 1024 // max number of fds in fd_set
 #endif
-#include <winsock2.h>
 #include <mswsock.h>
 #include <ws2tcpip.h>
 #else
@@ -33,9 +34,7 @@
 #include <net/if.h>
 #include <netinet/in.h>
 #include <ifaddrs.h>
-#endif
 
-#ifndef _WIN64
 typedef u_int SOCKET;
 #endif
 
