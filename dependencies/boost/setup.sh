e86349226c4392e202cc7dad9662e8726e7ebd90
laser9un@gmail.com <laser9un@gmail.com>
2025-02-08 02:27:24 +0300

    Change boost download link

--- dependencies/boost/setup.sh
+++ dependencies/boost/setup.sh
@@ -8,7 +8,7 @@ fi
 ROOT=$(pwd)
 VER=1_84_0
 
-wget -O ${ROOT}/boost_${VER}.tar.gz https://boostorg.jfrog.io/artifactory/main/release/1.84.0/source/boost_${VER}.tar.gz
+wget -O ${ROOT}/boost_${VER}.tar.gz https://archives.boost.io/release/1.84.0/source/boost_${VER}.tar.gz
 tar -xzf ${ROOT}/boost_${VER}.tar.gz
 mv ${ROOT}/boost_${VER} boost
 
