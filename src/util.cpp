8f5df3ee8944b797f7626feb6f560f58580080b5
laser9un@gmail.com <laser9un@gmail.com>
2025-08-22 15:15:10 +0300

    Fix JSON-RPC port in 42.conf

--- src/util.cpp
+++ src/util.cpp
@@ -1070,7 +1070,7 @@ void createConf()
 #endif
     pConf << "rpcuser=user\nrpcpassword="
             + randomStrGen(15)
-            + "\nrpcport=8344"
+            + "\nrpcport=2121"
             + "\n#(0=off, 1=on) daemon - run in the background as a daemon and accept commands"
             + "\ndaemon=0"
             + "\n#(0=off, 1=on) server - accept command line and JSON-RPC commands"
